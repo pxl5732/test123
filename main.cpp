2a541b7d17ed590742977d5608c32d6a0daa1ea3
pxl5732 <166149764+pxl5732@users.noreply.github.com>
2024-04-05 19:36:14 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    int age;
+
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    cout << "Your name: " << name << endl;
+    cout << "Your age: " << age << endl;
+
+    return 0;
+}
